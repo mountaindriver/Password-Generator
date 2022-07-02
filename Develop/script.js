53405bd27e74a0ca0220cb695512ce00deea8fca
mountaindriver <lsfreigenberg@outlook.com>
2022-07-01 20:49:22 -0600

    draft

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-const number = [1,2,3,4,5,6,7,8,9,0];
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", ",", "/", ":", ";", "<", ">", "?", "[", "]",];
-const characterCodes = Array.from(Array(26)).map( (_, i) => i + 97);
-const lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
-const uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
+const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let upperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase());
 
 
-function generatePassword() {
-  console.log("Hey! You cliked the button")
-  
-
-
-// 1. prompt user for the password criteria  
-//      a. password length 8-128
-//      b.Lowercase, uppercase, numbers, Symbols
-  let passwordlength = prompt("Please choose a password length between 8 and 128 characters", 8)
-  
-  let LowerCase = prompt("Would you like LOWERCASE letters?", true);
-  if (LowerCase !=null) {
-  }  
-
-  let UpperCase = prompt("Would you like UPPERCASE letters?", true);
-
-  let numbers = prompt("Would you like NUMBERS?", true);
-
-  let symbols = prompt("Would you like SYMBOLS?", true);
-
-
-
-// 2. Validate the input  
-// 3. Generate passowrd based on criteria
-for ( let i =0; i < characterAmount; i++) {
-  con
-}
-// 4. display password to the page
-
-
-
-return "Generated Password will go here!";
-}  
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var passwordText = document.querySelector("#password");
+    var passwordCharaters = [" "];
+
+  let charaterAmount = prompt("Length of your password 8-128?");
+  let LowerCase = prompt("Would you like LOWERCASE letters?", true);
+  let UpperCase = prompt("Would you like UPPERCASE letters?", true);
+  let num = prompt("Would you like NUMBERS?", true);
+  let symbols = prompt("Would you like SYMBOLS?", true);
   
+  
+  if (LowerCase == true){
+      passwordCharaters.push(lowerCaseLetters);
+    }
+    
+    if (UpperCase ==true){
+        passwordCharaters.push(upperCaseLetters);
+    }
+    
+    if (num == true){
+        passwordCharaters.push(number);
+    }
+    
+    if (symbols == true){
+        passwordCharaters.push(symbols);
+    }
+    
+    var password = generatePassword(){
+    //   Take form passwordcharater array at random till the amount is meet
+    };
+    
+    // Some way to stop the function if the number is below 8 or above 128
+
   passwordText.value = password;
-  
-}  
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+console.log(passwordCharaters);
